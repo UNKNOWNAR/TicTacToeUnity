e3f80ae8032c83fb3714274fe0486b8aa5e330f1
Aditya-Mukherjee-0806 <128787710+Aditya-Mukherjee-0806@users.noreply.github.com>
2023-06-02 00:32:56 +0530

    added player choice X or O in singleplayer

--- Scripts/SinglePlayerLogicScript.cs
+++ Scripts/SinglePlayerLogicScript.cs
@@ -7,9 +7,12 @@ public class SinglePlayerLogicScript : MonoBehaviour
     [SerializeField] private Text selectedDifficulty;
     [SerializeField] private Sprite X;
     [SerializeField] private Sprite O;
+    [SerializeField] private Sprite XWIN;
+    [SerializeField] private Sprite OWIN;
     [SerializeField] private Sprite defaultSprite;
     [SerializeField] private Text message;
     [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private XorOSplashScreen XorO;
 
     private string selectedDifficultyString;
     private TTTAI ai = new TTTAI();
@@ -19,20 +22,26 @@ public class SinglePlayerLogicScript : MonoBehaviour
     private int difficulty = 4;
     private int previousMove = 69;
     private int choice = (new System.Random().Next(2) + 1);
-    private char nextCharacter = 'O';
+    private char nextCharacter;//stores charcter for computers move and also the winning character
     private bool gameIsOver = false;
     private int winrange = 0;//contains a code example 13 start from 1 and increment with 3 times to get the win line
+
+    private Sprite playerChoiceSprite;
+    private char playerChoiceChar;
 
     private const string DIFFICULTY_SELECTED_STRING = "DifficultySelectedString";
     private const string DIFFICULTY_SELECTED_INTEGER = "DifficultySelectedInteger";
 
     private void Start()
     {
+        playerChoiceSprite = XorO.GetPlayerChoiceSprite();
+        playerChoiceChar = XorO.GetPlayerChoiceChar();
+        nextCharacter = playerChoiceChar == 'X' ? 'O' : 'X';
         fill();
         message.text = (choice == 1) ? "Computer's First Move" : "Your First Move";
         if (choice == 1)
         {
-            nextCharacter = 'X';
+            //nextCharacter = 'X';
             check1(); //To start the game when computer is supposed to give the first move
         }
         if (PlayerPrefs.HasKey(DIFFICULTY_SELECTED_STRING) && PlayerPrefs.HasKey(DIFFICULTY_SELECTED_INTEGER))
@@ -64,7 +73,9 @@ public class SinglePlayerLogicScript : MonoBehaviour
         message.text = null;
         int n = GetLastButtonClicked();
         button = GameObject.FindGameObjectWithTag(n.ToString()).GetComponent<Button>();
-        if (moveNumber % 2 == 0)
+        button.image.sprite = playerChoiceSprite;
+        play[n] = playerChoiceChar;
+        /*if (moveNumber % 2 == 0)
         {
             play[n] = 'X';
             button.image.sprite = X;
@@ -73,7 +84,7 @@ public class SinglePlayerLogicScript : MonoBehaviour
         {
             play[n] = 'O';
             button.image.sprite = O;
-        }
+        }*/
         button.enabled = false;
         previousMove = n;
         moveNumber++;
@@ -121,13 +132,11 @@ public class SinglePlayerLogicScript : MonoBehaviour
         
         if (store + choice == 2)
         {
-            Debug.Log(winrange);
             message.text = "Congratulations! You Won";
             moveNumber = 69;            
         }
         else if (store == 0 || store == 1)
         {
-            Debug.Log(winrange);
             message.text = "Computer Won";
             moveNumber = 69;
         }
@@ -138,18 +147,32 @@ public class SinglePlayerLogicScript : MonoBehaviour
         }
         if (moveNumber == 69)
         {
+            if (store == 0)
+                nextCharacter = 'X';
+            else if (store == 1)
+                nextCharacter = 'O';
             gameIsOver = true;
             onGameOver();
         }
     }
     private void onGameOver()
     {
+        int i = winrange / 10;
+        int d = winrange % 10;
+        int loopmove = 0;
         for (int j = 0; j < 9; j++)
-        {
+        { 
             button = GameObject.FindGameObjectWithTag(j.ToString()).GetComponent<Button>();
             if (button.image.sprite == defaultSprite)
-            {
                 button.interactable = false;                
+            else if (j == i && loopmove != 3&&winrange!=6)
+            {
+                if (nextCharacter == 'X')
+                    button.image.sprite = XWIN;
+                else if(nextCharacter == 'O')
+                    button.image.sprite = OWIN;
+                i += d;
+                loopmove++;
             }
         }
         gameOver();
--- Scripts/XorOSplashScreen.cs
+++ Scripts/XorOSplashScreen.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class XorOSplashScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject chooseXorOCanvas;
+    [SerializeField] private GameObject logicManager;
+    [SerializeField] private Sprite X_Sprite;
+    [SerializeField] private Sprite O_Sprite;
+    [SerializeField] private Button XorOChoiceButton;
+    [SerializeField] private Text playAsText;
+
+    private const char X = 'X';
+    private const char O = 'O';
+    private const string PLAY_AS = "Play As:   ";
+
+    private Sprite playerChoiceSprite;
+    private char playerChoiceChar;
+   public void OnXButtonClicked()
     {
-        
+        playerChoiceSprite = X_Sprite;
+        playerChoiceChar = X;
+        playAsText.text = PLAY_AS + playerChoiceChar;
+        XorOChoiceButton.image.sprite = playerChoiceSprite;
+        chooseXorOCanvas.SetActive(false);
+        logicManager.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+   public void OnOButtonClicked()
     {
-        
+        playerChoiceSprite = O_Sprite;
+        playerChoiceChar = O;
+        playAsText.text = PLAY_AS + playerChoiceChar;
+        XorOChoiceButton.image.sprite = playerChoiceSprite;
+        chooseXorOCanvas.SetActive(false);
+        logicManager.SetActive(true);
+    }
+
+    public void OnXorOChoiceButtonClicked()
+    {
+        chooseXorOCanvas.SetActive(true);
+    }
+
+    public Sprite GetPlayerChoiceSprite()
+    {
+        return playerChoiceSprite;
+    }
+
+    public char GetPlayerChoiceChar()
+    {
+        return playerChoiceChar;
     }
 }
